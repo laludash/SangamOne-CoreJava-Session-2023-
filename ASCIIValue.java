1fc751c3e3399e379245e99f8fd7f1a9a1377ee1
laludash <laludash6@gmail.com>
2023-10-22 09:06:12 +0530

    ASCII

---
+++ ASCIIValue.java
@@ -0,0 +1,23 @@
+package com.javaprogram.com;
+
+public class ASCIIValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(char uch = 'A'; uch <= 'Z'; uch++)
+		{
+			System.out.println(uch +"-"+(int) uch);
+		}
+		
+		
+		for(char lch = 'a'; lch <= 'z'; lch++)
+		{
+			System.out.println(lch + "-" +(int)lch);
+		}
+		
+		
+		
+	}
+
+}
